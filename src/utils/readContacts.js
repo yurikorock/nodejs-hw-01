0d6b06d1941af04fe9a7b6b8d227ab82864b45b5
Yura Shaplavsky <yurikorock777@gmail.com>
2025-06-21 15:07:14 +0300

    add readContacts

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,26 @@
 import { PATH_DB } from '../constants/contacts.js';
+import path from 'node:path';
+import fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pathToDb = path.resolve(__dirname, PATH_DB);
+
+export const readContacts = async () => {
+  try {
+    const fileContent = await fs.readFile(pathToDb, 'utf8');
+    console.log('Вміст файлу:', fileContent);
+    return JSON.parse(fileContent);
+  } catch (err) {
+    console.error('Помилка читання файлу:', err);
+    return null;
+    //   throw new Error('Не вдалося прочитати файл контактів');
+  }
+};
+
+// У файлі src / utils / readContacts.js опишіть утилітну функцію readContacts.
+// Вона має зчитувати вміст файлу src / db / db.json та повертати його.
+// Функція повинна коректно обробляти дані та забезпечувати правильне зчитування даних з файлу.
