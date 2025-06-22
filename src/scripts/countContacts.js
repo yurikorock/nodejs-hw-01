598b583dab89de33ce46557597d398f4194a6ae0
Yura Shaplavsky <yurikorock777@gmail.com>
2025-06-22 14:32:03 +0300

    create countContacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,17 @@
-export const countContacts = async () => {};
+import { getAllContacts } from './getAllContacts.js';
+
+export const countContacts = async () => {
+  const totalContacts = await getAllContacts();
+  return totalContacts.length;
+};
 
 console.log(await countContacts());
+
+// Крок 6
+
+// У файлі src / scripts / countContacts.js опишіть функцію countContacts.
+// Вона має повертати кількість контактів в масиві у файлі src / db / db.json.
+// Переконайтесь, що функція точно рахує кількість контактів.
+
+// Перевірити роботу функції можна виконавши команду npm run count,
+// яка буде виконувати код у файлі src / scripts / countContacts.js.
